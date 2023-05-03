8c2189c1663cc3cd08ffd1fc20658d9d46c5c823
Faustine67 <michelfaustine@gmail.com>
2023-05-03 16:46:55 +0200

    Debeug Login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,46 +15,46 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: 'security/home', name: 'home')]
-    public function home(EntityManagerInterface $entityManager) {
-        if ($this->getUser()) {
+    // #[Route(path: 'security/home', name: 'home')]
+    // public function home(EntityManagerInterface $entityManager) {
+    //     if ($this->getUser()) {
 
-            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+    //         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-            $stagiaireRepo = $entityManager->getRepository(Stagiaire::class);
-            $formateurRepo = $entityManager->getRepository(Formateur::class);
-            $formationRepo = $entityManager->getRepository(Formation::class);
-            $sessionRepo = $entityManager->getRepository(Session::class);
+    //         $stagiaireRepo = $entityManager->getRepository(Stagiaire::class);
+    //         $formateurRepo = $entityManager->getRepository(Formateur::class);
+    //         $formationRepo = $entityManager->getRepository(Formation::class);
+    //         $sessionRepo = $entityManager->getRepository(Session::class);
     
-            $stagiaireCount = $stagiaireRepo->count([]);
-            $formateurCount = $formateurRepo->count([]);
-            $formationCount = $formationRepo->count([]);
+    //         $stagiaireCount = $stagiaireRepo->count([]);
+    //         $formateurCount = $formateurRepo->count([]);
+    //         $formationCount = $formationRepo->count([]);
 
-            $incomingSessions = $sessionRepo->findIncomingSessions();
-            $inProgressSessions =  $sessionRepo->findInProgressSessions();
-            $passedSessions =  $sessionRepo->findPassedSessions();
+    //         $incomingSessions = $sessionRepo->findIncomingSessions();
+    //         $inProgressSessions =  $sessionRepo->findInProgressSessions();
+    //         $passedSessions =  $sessionRepo->findPassedSessions();
 
-            return $this->render('security/home.html.twig', [
-                'stagiaireCount' => $stagiaireCount,
-                'formateurCount' => $formateurCount,
-                'formationCount' => $formationCount,
-                'incSessions' => $incomingSessions,
-                'inProgressSessions' => $inProgressSessions,
-                'passedSessions' => $passedSessions
-            ]);
-        }
-        else {
-            return $this->render('/home.html.twig');
-        }
-    }
+    //         return $this->render('security/home.html.twig', [
+    //             'stagiaireCount' => $stagiaireCount,
+    //             'formateurCount' => $formateurCount,
+    //             'formationCount' => $formationCount,
+    //             'incSessions' => $incomingSessions,
+    //             'inProgressSessions' => $inProgressSessions,
+    //             'passedSessions' => $passedSessions
+    //         ]);
+    //     }
+    //     else {
+    //         return $this->render('security/home.html.twig');
+    //     }
+    // }
 
    
     #[Route(path: 'security/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -70,33 +70,33 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
-    #[Route(path: 'security/show', name: 'show')]
-    public function show(EntityManagerInterface $entityManager): Response
-    {
+    // #[Route(path: 'security/show', name: 'show')]
+    // public function show(EntityManagerInterface $entityManager): Response
+    // {
 
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+    //     $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        $stagiaireRepo = $entityManager->getRepository(Stagiaire::class);
-        $trainerRepo = $entityManager->getRepository(Trainer::class);
-        $trainingRepo = $entityManager->getRepository(Training::class);
-        $sessionRepo = $entityManager->getRepository(Session::class);
+    //     $stagiaireRepo = $entityManager->getRepository(Stagiaire::class);
+    //     $trainerRepo = $entityManager->getRepository(Trainer::class);
+    //     $trainingRepo = $entityManager->getRepository(Training::class);
+    //     $sessionRepo = $entityManager->getRepository(Session::class);
 
-        $stagiaireCount = $stagiaireRepo->count([]);
-        $trainerCount = $trainerRepo->count([]);
-        $trainingCount = $trainingRepo->count([]);
+    //     $stagiaireCount = $stagiaireRepo->count([]);
+    //     $trainerCount = $trainerRepo->count([]);
+    //     $trainingCount = $trainingRepo->count([]);
 
-        $incomingSessions = $sessionRepo->findIncomingSessions();
-        $inProgressSessions =  $sessionRepo->findInProgressSessions();
-        $passedSessions =  $sessionRepo->findPassedSessions();
+    //     $incomingSessions = $sessionRepo->findIncomingSessions();
+    //     $inProgressSessions =  $sessionRepo->findInProgressSessions();
+    //     $passedSessions =  $sessionRepo->findPassedSessions();
 
-        return $this->render('security/home.html.twig', [])
-        //     'stagiaireCount' => $stagiaireCount,
-        //     'trainerCount' => $trainerCount,
-        //     'trainingCount' => $trainingCount,
-        //     'incSessions' => $incomingSessions,
-        //     'inProgressSessions' => $inProgressSessions,
-        //     'passedSessions' => $passedSessions
-        // ]);
-    ;
-    }
+    //     return $this->render('security/home.html.twig', [])
+    //     //     'stagiaireCount' => $stagiaireCount,
+    //     //     'trainerCount' => $trainerCount,
+    //     //     'trainingCount' => $trainingCount,
+    //     //     'incSessions' => $incomingSessions,
+    //     //     'inProgressSessions' => $inProgressSessions,
+    //     //     'passedSessions' => $passedSessions
+    //     // ]);
+    // ;
+    // }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,9 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -37,6 +40,20 @@ class UserRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
+    }
+
+        /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newHashedPassword);
+
+        $this->save($user, true);
     }
 
 //    /**
